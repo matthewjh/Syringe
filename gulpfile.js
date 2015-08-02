d7ea5908420688ec4d0d44a7ce5d8d11a3608cff
Matthew Hill <matthew.jh@outlook.com>
2015-08-02 21:47:53 +0100

    uses fileGlob when building ts

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,17 @@ var ts = require('gulp-typescript');
 var rimraf = require('gulp-rimraf');
 var exec = require('child_process').exec;
 
-var typescriptProject = ts.createProject('tsconfig.json');
-
 gulp.task('clean-build-folder', function () {
   return gulp.src('built/**/*', { read: false })
     .pipe(rimraf());
 });
 
 gulp.task('build', ['clean-build-folder'], function (done) {
-  exec('tsc --p .', function (error, stdout, stderr) {
-    if (stdout) {
-      console.log(stdout);
-    }
-
-    if (stderr) {
-      console.error(stderr);
-    }
-
-    done(null);
-  });
+  var tsconfig = require("./tsconfig.json");
+  var filesGlob = tsconfig.filesGlob;
+  return gulp.src(filesGlob)
+      .pipe(ts(tsconfig.compilerOptions))
+      .pipe(gulp.dest(tsconfig.compilerOptions.outDir));
 });
 
 gulp.task('unit', ['build'], function () {
