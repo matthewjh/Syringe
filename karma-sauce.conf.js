015dec585eb8f50512c87087c9f7616ce5f75211
Matthew Hill <matthew.jh@outlook.com>
2015-09-24 19:44:48 +0100

    feat(provider/class): adds support for es6 classes

--- karma-sauce.conf.js
+++ karma-sauce.conf.js
@@ -2,16 +2,34 @@ module.exports = function (config) {
   require('./karma.conf')(config);
 
   var customLaunchers = {
-    sl_chrome: {
+    sl_chrome_35: {
       base: 'SauceLabs',
       browserName: 'chrome',
       platform: 'Windows 7',
       version: '35'
     },
+    sl_chrome_45: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X 10.10',
+      version: '45.0'
+    },
+    sl_chrome_beta: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X 10.10',
+      version: 'beta'
+    },
     sl_firefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
       version: '30'
+    },
+    sl_firefox_40: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'OS X 10.10',
+      version: '40.0'
     },
     sl_ie_11: {
       base: 'SauceLabs',
@@ -30,6 +48,12 @@ module.exports = function (config) {
       browserName: 'internet explorer',
       platform: 'Windows 2008',
       version: '9'
+    },
+    sl_edge: {
+      base: 'SauceLabs',
+      browserName: 'microsoftedge',
+      platform: 'Windows 10',
+      version: '20.10240'
     }
   };
 
@@ -37,6 +61,7 @@ module.exports = function (config) {
     sauceLabs: {
       testName: 'Syringe.ts tests'
     },
+    captureTimeout: 200000,
     customLaunchers: customLaunchers,
     browsers: Object.keys(customLaunchers),
     reporters: ['dots', 'saucelabs'],
